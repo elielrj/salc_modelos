5dcf3bffc7b54b3cbdc7a7168e2575b658b9a6fc
elielrj <elielrj@gmail.com>
2023-05-24 15:47:16 -0300

    Acrescentado modelos de caronas aos modelos

---
+++ carona/8666/8666.php
@@ -0,0 +1,11 @@
+<div class='column'>
+    <!-- Div Lei 8.666 compras comuns -->
+    <h2>Lei 8.666/1993 e 10.520/2002</h2>
+    <ul>
+        <li class="list-style">
+            <div>
+                <?php include_once('documentos_comuns.html'); ?> </br>
+            </div>
+        </li>
+    </ul>
+</div>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="lib/sb-admin-2.css">
-        <title>Modelos</title>
+        <title>Modelos - SALC</title>
     </head>
     <body>
             <div class="text-center p-3" style="width: 50rem;"> 
@@ -18,11 +18,30 @@
                     <container class='column'>
                         <div class="card text-center text-white bg-success" style="heidth: 400px;">
                             <!-- Nova Lei -->                
-                            <a class="btn p-3" href='14133/' role="button">Lei 14.133/21</a> 
+                            <a class="btn p-3" href='pregao/14133/' role="button">Lei 14.133/21</a> 
                         </div>  
                         <div class="card text-center text-white bg-danger" style="heidth: 400px;">
                             <!-- Lei 8.666 serviços comuns-->
-                            <a class="btn p-3" href='8666/' role="button">Lei 8.666/93 e 10.520/02</a> 
+                            <a class="btn p-3" href='pregao/8666/' role="button">Lei 8.666/93 e 10.520/02</a> 
+                        </div>
+                    </container>
+                                
+            </div>
+
+            <div class="text-center p-3" style="width: 50rem;"> 
+                <!-- Div Modelos de pregões -->
+                <h1>Modelos de "Carona"</ha1></br></br>      
+               
+                         
+                    
+                    <container class='column'>
+                        <div class="card text-center text-white bg-success" style="heidth: 400px;">
+                            <!-- Nova Lei -->                
+                            <a class="btn p-3" href='carona/14133/' role="button">Lei 14.133/21</a> 
+                        </div>  
+                        <div class="card text-center text-white bg-success" style="heidth: 400px;">
+                            <!-- Lei 8.666 serviços comuns-->
+                            <a class="btn p-3" href='carona/8666/' role="button">Lei 8.666/93 e 10.520/02</a> 
                         </div>
                     </container>
                                 

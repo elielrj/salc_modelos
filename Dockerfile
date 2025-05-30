5842c1f70f46428394465dfe7b18d607e6d5c26b
Eliel de Souza Faria <elielrj@gmail.com>
2025-05-30 13:17:49 -0300

    Ajuste de docker file e docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,1 @@
 FROM php:8.3-apache
-COPY . /var/www/html
--- dispensa/index.php
+++ dispensa/index.php
@@ -32,14 +32,7 @@
                 </p>
             </li>
 
-            
-        </ol>
-
-
-        <p>Documentos do Demandante (p/ selecionar um fornecedor)</p>
-        <ol>
-
-            <li>
+             <li>
                 <p>
                     <a class="btn btn-success"
                         href="https://docs.google.com/document/d/1FZLuXfwmd6V4hROiAN9AXoMERJZIWY6o/edit?usp=sharing&ouid=102031952743882595527&rtpof=true&sd=true"
@@ -47,26 +40,39 @@
                     </a>
                 </p>
             </li>
+            
+        </ol>
 
+
+        <p>Documentos do Demandante (p/ selecionar um fornecedor)</p>
+        <ol>
+        
             <li>
                 <p>
                     <a class="btn btn-success"
-                        href="https://docs.google.com/document/d/1aV7iMs53cNdcRdbBvj0gZDKazIThPEsb/edit?usp=sharing&ouid=102031952743882595527&rtpof=true&sd=true"
-                        role="button">Termo de Referência P/ COMPRAS p/ permanente e consumo (Demandante)
+                        href="https://drive.google.com/file/d/1vRV3S_CNKVpPCremnkJYIKgntzrcpxx5/view?usp=drive_link"
+                        role="button">Termo de Abertura
                     </a>
                 </p>
             </li>
 
-            <li>
+                <li>
                 <p>
                     <a class="btn btn-success"
                         href="https://docs.google.com/document/d/1X5UpkGmGwZc5SYslsuu_gaubAnNvz36n/edit?usp=sharing&ouid=102031952743882595527&rtpof=true&sd=true"
-                        role="button">Termo de Referência p/ SERVIÇOS (Demandante)
+                        role="button">Termo de Referência p/ SERVIÇOS
                     </a>
                 </p>
             </li>
 
-
+                        <li>
+                <p>
+                    <a class="btn btn-success"
+                        href="https://docs.google.com/document/d/1aV7iMs53cNdcRdbBvj0gZDKazIThPEsb/edit?usp=sharing&ouid=102031952743882595527&rtpof=true&sd=true"
+                        role="button">Termo de Referência P/ COMPRAS
+                    </a>
+                </p>
+            </li>
 
         </ol>
 

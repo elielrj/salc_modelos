dadb50eb04a24850f409121b261d19884b24e6f5
Eliel de Souza Faria <elielrj@gmail.com>
2025-06-26 09:58:29 -0300

    Modificado para CND a biblioteca do BootStrap

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="./lib/sb-admin-2.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <title>Modelos</title>
 </head>
 
@@ -13,7 +13,7 @@
     <div class="container py-5 text-center">
 
         <div class="row row-cols-1 row-cols-md-1 row-cols-lg-1 g-1">
-            <div class="card alert-secondary" style="margin-bottom: 20px">
+            <div class="card bg-success-subtle" style="margin-bottom: 20px">
                 <div class="card-body">
                     <div>
                         <h2 class="card-title">Pregão</h2>
@@ -21,12 +21,12 @@
                     </div>
                     <br>
                     <div>
-                        <a class="card btn p-3 alert-primary" href='pregao/' role="button">PREGÃO</a>
+                        <a class="card btn p-3 bg-light-subtle" href='pregao/' role="button">PREGÃO</a>
                     </div>
                 </div>
             </div>
 
-            <div class="card alert-secondary" style="margin-bottom: 20px">
+            <div class="card bg-warning-subtle" style="margin-bottom: 20px">
                 <div class="card-body">
                     <div>
                         <h2 class="card-title">Pregão CARONA</h2>
@@ -34,12 +34,12 @@
                     </div>
                     <br>
                     <div>
-                        <a class="card btn p-3 alert-primary" href='carona/' role="button">CARONA</a>
+                        <a class="card btn p-3 bg-light-subtle" href='carona/' role="button">CARONA</a>
                     </div>
                 </div>
             </div>
 
-            <div class="card alert-secondary" style="margin-bottom: 20px">
+            <div class="card bg-danger-subtle" style="margin-bottom: 20px">
                 <div class="card-body">
                     <div>
                         <h2 class="card-title">Dispensa Licitação</h2>
@@ -47,12 +47,12 @@
                     </div>
                     <br>
                     <div>
-                        <a class="card btn p-3 alert-primary" href='dispensa/' role="button">DISPENSA</a>
+                        <a class="card btn p-3 bg-light-subtle" href='dispensa/' role="button">DISPENSA</a>
                     </div>
                 </div>
             </div>
 
-            <div class="card alert-secondary" style="margin-bottom: 20px">
+            <div class="card bg-primary-subtle" style="margin-bottom: 20px">
                 <div class="card-body">
                     <div>
                         <h2 class="card-title">Requisitória</h2>
@@ -60,12 +60,12 @@
                     </div>
                     <br>
                     <div>
-                        <a class="card btn p-3 alert-primary" href='requisitoria/' role="button">REQUISITÓRIA</a>
+                        <a class="card btn p-3 bg-light-subtle" href='requisitoria/' role="button">REQUISITÓRIA</a>
                     </div>
                 </div>
             </div>
 
-            <div class="card alert-secondary" style="margin-bottom: 20px">
+            <div class="card bg-info-subtle" style="margin-bottom: 20px">
                 <div class="card-body">
                     <div>
                         <h2 class="card-title">BIZUS</h2>
